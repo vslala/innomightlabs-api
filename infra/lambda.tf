9464677246cc3968f5967ec2c42709cead53eaec
vslala <varunshrivastava007@gmail.com>
2025-07-09 00:27:36 +0100

    refresh lambda on terraform deploy to use latest image

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,11 +1,18 @@
+# Data source to get the latest ECR image
+data "aws_ecr_image" "innomightlabs_api" {
+  repository_name = aws_ecr_repository.innomightlabs_api.name
+  image_tag       = "latest"
+}
+
 # Lambda Function
 resource "aws_lambda_function" "innomightlabs_api" {
-  function_name = "${var.project_name}-lambda"
-  role          = aws_iam_role.lambda_execution.arn
-  package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.innomightlabs_api.repository_url}:latest"
-  timeout       = 60
-  memory_size   = 1024
+  function_name    = "${var.project_name}-lambda"
+  role            = aws_iam_role.lambda_execution.arn
+  package_type    = "Image"
+  image_uri       = "${aws_ecr_repository.innomightlabs_api.repository_url}:latest"
+  source_code_hash = data.aws_ecr_image.innomightlabs_api.image_digest
+  timeout         = 60
+  memory_size     = 1024
 
   vpc_config {
     subnet_ids         = [aws_subnet.private_a.id, aws_subnet.private_b.id]

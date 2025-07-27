57370036c469988850846481f2dd929d4800241a
vslala <varunshrivastava007@gmail.com>
2025-07-27 23:26:28 +0100

    enhance fetch_webpage tool to fetch and download pdf file as well

--- app/chatbot/chatbot_models.py
+++ app/chatbot/chatbot_models.py
@@ -1,12 +1,14 @@
+import os
 import asyncio
 from datetime import datetime, timezone
 from enum import Enum
-from typing import Any, Generator
+from typing import Any, ClassVar, Generator
 
 from pydantic import BaseModel, ConfigDict, Field
 
 from app.common.models import Role, StreamStep
 from app.conversation.messages.message_models import AgentVersion
+import shutil
 
 
 class ActionResult(BaseModel):
@@ -65,7 +67,10 @@ class AgentMessage(BaseModel):
     def get_formatted_prompt(self) -> str:
         ts_str = self.timestamp.strftime("%Y-%m-%d %H:%M")
         role_cap = self.role.value.capitalize()
-        return f"[{role_cap}  - {ts_str}]  {self.message}"
+        return f"""
+## {role_cap} - {ts_str}
+{self.message}
+"""
 
 
 class Phase(Enum):
@@ -73,30 +78,41 @@ class Phase(Enum):
     NEED_TOOL = "need_tool"
 
 
+class MemoryManagementConfig(BaseModel):
+    CONTEXT_LENGTH: ClassVar[int] = 100000
+    SYSTEM_PROMPT_LIMIT: ClassVar[int] = int(CONTEXT_LENGTH * 0.20)
+    LOCAL_MEMORY_LIMIT: ClassVar[int] = int(CONTEXT_LENGTH * 0.50)
+    USER_MESSAGE_LIMIT: ClassVar[int] = int(CONTEXT_LENGTH * 0.1)
+    CONVERSATION_PAGE_SIZE: ClassVar[int] = 10
+    OBSERVATIONS_PAGE_SIZE: ClassVar[int] = 10
+    AVERAGE_TOKEN_SIZE: ClassVar[int] = 4
+
+
 class AgentState(BaseModel):
     """State for the chat agent workflow."""
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
+    epochs: int = Field(default=0)
 
     messages: list[AgentMessage]
     user_message: str
     agent_message: str = Field(default="")
     prompt: str = Field(default="")
+    filepaths: list[str] = Field(default=[])
+
+    # Memory Segments
+    system_prompt: str = Field(default="")
+    local_memory: str = Field(default="")
+    recent_conversation_history: str = Field(default="")
+    current_conversation_history_page: int = Field(default=1)
 
     # Multi-step reasoning fields
     phase: Phase = Field(default=Phase.NEED_FINAL)
-    analysis: str = Field(default="")
     thought: AgentThought | None = Field(default=None)
     observations: list[ActionResult] = Field(default=[])
 
-    reasoning: str = Field(default="")
-    synthesis: str = Field(default="")
-
     # Quality assurance fields
     draft_response: str = Field(default="")
-    evaluation: str = Field(default="")
-    needs_refinement: bool = Field(default=True)
-    refinement_count: int = Field(default=0)
 
     # Error handling
     retry: int = Field(default=0)
@@ -104,22 +120,65 @@ class AgentState(BaseModel):
 
     stream_queue: asyncio.Queue = Field(default_factory=lambda: asyncio.Queue(maxsize=0))
 
-    def build_conversation_history(self, limit: int = 20) -> str:
+    def build_conversation_history(self, curr_page: int = 1) -> str:
         """Build the conversation history from the state."""
-        self.messages.sort(key=lambda msg: msg.timestamp)
-        return "\n".join(msg.get_formatted_prompt() for msg in self.messages[-limit:])
+        page_size = MemoryManagementConfig.CONVERSATION_PAGE_SIZE
+        total_pages = len(self.messages) // page_size + 1
+        message_start = (page_size * curr_page) - page_size
+        message_end = message_start + page_size + 1
 
-    def build_observation(self, limit: int = 5) -> str:
+        curr_messages = self.messages[message_start:message_end]
+        curr_messages.sort(key=lambda msg: msg.timestamp)
+
+        summary = f"""
+Page Size: {page_size}
+Total Pages: {total_pages}
+Current Page: {curr_page}
+"""
+        messages = "\n".join(msg.get_formatted_prompt() for msg in curr_messages)
+        return f"""
+## CONVERSATION HISTORY
+{summary}
+
+{messages}
+"""
+
+    def build_observation(self, curr_page: int = 1) -> str:
         """Build the observation from the state."""
         if not self.observations:
             return ""
+        page_size = MemoryManagementConfig.OBSERVATIONS_PAGE_SIZE
         n = len(self.observations)
-        observations = self.observations[-limit:] if n > 3 else self.observations
-        prompt = "\n# OBSERVATIONS\n\n"
-        prompt += "Below are the observations/result of your previous actions:\n"
-        for idx, obs in enumerate(observations):
-            prompt += f"## Observation {(idx + 1)}.\n"
+        total_pages = n // page_size + 1
+        start = (page_size * curr_page) - page_size
+        end = start + page_size + 1
+
+        curr_obvs = self.observations[start:end]
+
+        prompt = f"""\n## OBSERVATIONS
+### Summary:
+Total Pages: {total_pages}
+Page Size: {page_size}
+Current Page: {curr_page}
+
+### Results
+Below are the observations/result of your previous actions:\n
+"""
+        for idx, obs in enumerate(curr_obvs):
+            prompt += f"### Result {(idx + 1)}.\n"
             prompt += f"{obs}\n\n"
+        return prompt
+
+    def list_available_temp_files(self) -> str:
+        """List the available temporary files from the state."""
+        if not self.filepaths:
+            return ""
+        paths = "\n-".join(self.filepaths)
+        prompt = f"""
+### TEMP FILES
+Below are the temporary files you have access to in the current session:
+{paths}
+"""
         return prompt
 
     def get_error_message(self) -> Generator[str, None, None]:
@@ -128,6 +187,45 @@ class AgentState(BaseModel):
         else:
             yield f"Got error: {self.error_message}"
             self.error_message = None
+
+    def add_system_prompt(self, sys_prompt: str) -> None:
+        """Add the system prompt to the state."""
+        if len(sys_prompt) // MemoryManagementConfig.AVERAGE_TOKEN_SIZE > MemoryManagementConfig.SYSTEM_PROMPT_LIMIT:
+            raise ValueError("System prompt is too long")
+
+        self.system_prompt = sys_prompt
+
+    def build_prompt(self) -> str:
+        """Build the prompt from the state."""
+        self.local_memory = f"""
+# LOCAL MEMORY
+{self.list_available_temp_files()}
+{self.build_conversation_history()}
+{self.build_observation()}
+## CURRENT USER MESSAGE
+{self.user_message}
+"""
+        local_memory_summary = f"""
+## SUMMARY
+Total Pages: {len(self.messages) // MemoryManagementConfig.CONVERSATION_PAGE_SIZE + 1}
+Current Page: {self.current_conversation_history_page}
+Local Memory Limit: {MemoryManagementConfig.LOCAL_MEMORY_LIMIT}
+Current Local Memory Used: {len(self.local_memory) // MemoryManagementConfig.AVERAGE_TOKEN_SIZE}
+"""
+
+        prompt = f"""
+{self.system_prompt}
+{self.local_memory}
+{local_memory_summary}
+"""
+        self.prompt = prompt
+        self.epochs += 1
+
+        shutil.rmtree("/tmp/prompts")
+        os.makedirs("/tmp/prompts", exist_ok=True)
+        with open(f"/tmp/prompts/p_{self.epochs}.md", "w") as f:
+            f.write(prompt)
+        return prompt
 
 
 # Requests
--- app/chatbot/workflows/helpers/tools.py
+++ app/chatbot/workflows/helpers/tools.py
@@ -11,6 +11,7 @@ import wikipedia
 from app.chatbot.chatbot_models import Action, ActionResult, AgentState, StreamChunk
 from app.common.models import StreamStep
 from contextlib import redirect_stdout
+from pdfminer.high_level import extract_text
 
 _shared_ns: dict = {}
 
@@ -22,22 +23,45 @@ available_actions = [
         description="If the response you are sending is too large you could use this tool to send it in multiple go. Sends the intermediate response to the user.",
         params={"text": "string"},
     ),
-    Action(name="final_response", description="Sends the final response to the user from the params.", params={"text": "string"}),
+    Action(
+        name="final_response",
+        description="""
+Sends the final response to the user in two ways:
+- text: If text is provided then the response is directly sent from the text
+- filepath: If filepath is provided (usually to send large response), then Krishna (you) can choose to write it in a disk-memory and then send the path to that file,
+    and the content of that file will be sent to the user
+""",
+        params={"text?": "string", "filepath?": "string"},
+    ),
     Action(name="wikipedia_search", description="Search Wikipedia and return a short summary.", params={"query": "string"}),
     Action(
         name="fetch_webpage",
-        description="Fetches the webpage and saves it in local temporary memory and adds the path of the file to observations for later processing at will using python code",
+        description="""
+Fetches the webpage and write the data in the disk memory and adds the file path to the observatio/result. 
+The file can be accessed by other tools or if you want to access parts of it you can use python code to do that.
+""",
         params={"url": "string"},
     ),
     Action(
-        name="write_data_to_temp_memory",
+        name="write_data_to_disk_memory",
         description="""
-        Writes the data to a temp file for later use
-            data: actual data to write
-            filename_prefix (optional): use it to understand what the file content is about
-            filepath (optional): if the filepath is provided then it appends to the same file
-        """,
+Writes the data to a file on disk for later use. It works in the following ways:
+data: actual data to write
+filename_prefix (optional): use it to understand what the file content is about
+    - You DO NOT need to provide filename_prefix if you are appending to the same file.
+filepath (optional):
+    - if filepath is provided then the content from `params.data` is appended to the same file.
+    - if filepath is not provided then a new file will be created everytime.
+""",
         params={"data": "string", "filename_prefix?": "string", "filepath?": "string"},
+    ),
+    Action(
+        name="read_data_from_disk_memory",
+        description="""
+        Reads the data from a file on disk and adds it to local-memory action results
+            filepath: reads the data from the provided filepath
+        """,
+        params={"filepath": "string"},
     ),
 ]
 
@@ -51,7 +75,21 @@ async def list_tools_tool(state: AgentState) -> ActionResult:
     return ActionResult(thought=state.thought.thought, action="list_tools", result=json.dumps([str(tool) for tool in tools], indent=2))
 
 
-async def write_data_to_temp_memory(state: AgentState) -> ActionResult:
+async def read_data_from_disk_memory(state: AgentState) -> ActionResult:
+    if not state.thought or (state.thought and state.thought.action.name != "read_data_from_disk_memory"):
+        return ActionResult(thought="", action="None", result="")
+
+    state.stream_queue.put_nowait(StreamChunk(content=state.thought.thought, step=StreamStep.ANALYSIS, step_title="Reading into local memory"))
+
+    filepath = state.thought.action.params.get("filepath")
+    if not filepath:
+        return ActionResult(thought=state.thought.thought, action=state.thought.action.name, result="Require filepath to load memory")
+    with open(filepath, "r", encoding="utf-8") as f:
+        data = f.read()
+        return ActionResult(thought=state.thought.thought, action=state.thought.action.name, result=data)
+
+
+async def write_data_to_disk_memory(state: AgentState) -> ActionResult:
     """
     Tool: writes the data to a temp file in the system temp directory so it can be retrieved later
     Params: {
@@ -61,7 +99,7 @@ async def write_data_to_temp_memory(state: AgentState) -> ActionResult:
     }
     Returns the path of the file as observation
     """
-    if not state.thought or (state.thought and state.thought.action.name != "write_data_to_temp_memory"):
+    if not state.thought or (state.thought and state.thought.action.name != "write_data_to_disk_memory"):
         return ActionResult(thought="", action="None", result="")
 
     state.stream_queue.put_nowait(StreamChunk(content=state.thought.thought, step=StreamStep.ANALYSIS, step_title="Writing to temp memory"))
@@ -84,39 +122,122 @@ async def write_data_to_temp_memory(state: AgentState) -> ActionResult:
     with open(path, mode, encoding="utf-8") as f:
         f.write(data)
 
+    state.filepaths.append(path)
     return ActionResult(thought=state.thought.thought, action=state.thought.action.name, result=json.dumps({"filepath": path}))
+
+
+# async def download_webpage_by_url(state: AgentState) -> ActionResult:
+#     """
+#     Tool: fetch the URL in state.thought.params["url"], scrape out the text,
+#     and saves it in the temp file
+#     Returns the path of the file as observation
+#     """
+#     if not state.thought or (state.thought and state.thought.action.name != "fetch_webpage"):
+#         return ActionResult(thought="", action="None", result="")
+
+#     state.stream_queue.put_nowait(StreamChunk(content=f"{state.thought.thought}", step=StreamStep.ANALYSIS, step_title="Fetching Webpage"))
+#     url = state.thought.action.params["url"]
+#     # 1) fetch HTML
+#     async with aiohttp.ClientSession() as sess:
+#         async with sess.get(url) as resp:
+#             await state.stream_queue.put(StreamChunk(content=f"Fetching {url}...", step=StreamStep.ANALYSIS, step_title="Fetching Webpage"))
+#             html = await resp.text()
+
+#     # 2) extract plain text
+#     soup = BeautifulSoup(html, "html.parser")
+#     text = soup.get_text(separator=" ", strip=True)
+
+#     # 2) Create a temp file in the system temp directory
+#     #    delete=False so it sticks around after closing
+#     fd, path = tempfile.mkstemp(suffix=".html", prefix="page_", dir=None)
+#     os.close(fd)
+
+#     # 3) Write the HTML
+#     with open(path, "w", encoding="utf-8") as f:
+#         f.write(text)
+#     return ActionResult(thought=state.thought.thought, action=state.thought.action.name, result=json.dumps({"url": url, "filepath": path}))
 
 
 async def download_webpage_by_url(state: AgentState) -> ActionResult:
     """
-    Tool: fetch the URL in state.thought.params["url"], scrape out the text,
-    and saves it in the temp file
-    Returns the path of the file as observation
+    Tool: fetch the URL in state.thought.params["url"], decide if it's HTML or PDF,
+    extract plain text, save it in a temp file (and PDF in a temp file if needed),
+    and return the filepath(s) in the result.
     """
-    if not state.thought or (state.thought and state.thought.action.name != "fetch_webpage"):
+    if not state.thought or state.thought.action.name != "fetch_webpage":
         return ActionResult(thought="", action="None", result="")
 
-    state.stream_queue.put_nowait(StreamChunk(content=f"{state.thought.thought}", step=StreamStep.ANALYSIS, step_title="Fetching Webpage"))
     url = state.thought.action.params["url"]
-    # 1) fetch HTML
+    await state.stream_queue.put(StreamChunk(content=f"Fetching {url}…", step=StreamStep.ANALYSIS, step_title="Fetching Webpage"))
+
     async with aiohttp.ClientSession() as sess:
         async with sess.get(url) as resp:
-            await state.stream_queue.put(StreamChunk(content=f"Fetching {url}...", step=StreamStep.ANALYSIS, step_title="Fetching Webpage"))
-            html = await resp.text()
+            content_type = resp.headers.get("Content-Type", "").lower()
+            is_pdf = "application/pdf" in content_type or url.lower().endswith(".pdf")
 
-    # 2) extract plain text
-    soup = BeautifulSoup(html, "html.parser")
-    text = soup.get_text(separator=" ", strip=True)
+            if is_pdf:
+                # 1) Download PDF bytes
+                pdf_bytes = await resp.read()
 
-    # 2) Create a temp file in the system temp directory
-    #    delete=False so it sticks around after closing
-    fd, path = tempfile.mkstemp(suffix=".html", prefix="page_", dir=None)
-    os.close(fd)
+                # 2) Write to a temp PDF file
+                pdf_fd, pdf_path = tempfile.mkstemp(suffix=".pdf", prefix="page_", dir=None)
+                os.close(pdf_fd)
+                with open(pdf_path, "wb") as f_pdf:
+                    f_pdf.write(pdf_bytes)
+                await state.stream_queue.put(StreamChunk(content="Saved PDF to temp file, extracting text…", step=StreamStep.SYNTHESIS, step_title=StreamStep.SYNTHESIS.value))
 
-    # 3) Write the HTML
-    with open(path, "w", encoding="utf-8") as f:
-        f.write(text)
-    return ActionResult(thought=state.thought.thought, action=state.thought.action.name, result=json.dumps({"url": url, "filepath": path}))
+                # 3) Extract text with pdfminer
+                text = extract_text(pdf_path)
+
+                # 4) Write extracted text to a temp .txt file
+                txt_fd, txt_path = tempfile.mkstemp(suffix=".txt", prefix="page_", dir=None)
+                os.close(txt_fd)
+                with open(txt_path, "w", encoding="utf-8") as f_txt:
+                    f_txt.write(text)
+
+                await state.stream_queue.put(StreamChunk(content="Extraction complete.", step=StreamStep.ANALYSIS, step_title=StreamStep.SYNTHESIS.value))
+                output_paths = {
+                    "url": url,
+                    "pdf_filepath": pdf_path,
+                    "text_filepath": txt_path,
+                }
+                state.filepaths.append(json.dumps(output_paths, indent=2))
+                return ActionResult(
+                    thought=state.thought.thought,
+                    action=state.thought.action.name,
+                    result=json.dumps(output_paths),
+                )
+
+            else:
+                # HTML path (unchanged)
+                html = await resp.text()
+                soup = BeautifulSoup(html, "html.parser")
+                text = soup.get_text(separator=" ", strip=True)
+
+                html_fd, html_path = tempfile.mkstemp(suffix=".html", prefix="page_", dir=None)
+                os.close(html_fd)
+                with open(html_path, "w", encoding="utf-8") as f_html:
+                    f_html.write(text)
+
+                state.filepaths.append(
+                    json.dumps(
+                        {
+                            "url": url,
+                            "filepath": html_path,
+                        },
+                        indent=2,
+                    )
+                )
+                return ActionResult(
+                    thought=state.thought.thought,
+                    action=state.thought.action.name,
+                    result=json.dumps(
+                        {
+                            "url": url,
+                            "filepath": html_path,
+                        }
+                    ),
+                )
 
 
 async def python_code_runner_tool(state: AgentState) -> ActionResult:
@@ -192,15 +313,27 @@ async def wikipedia_search_tool(state: AgentState) -> ActionResult:
 async def final_response_tool(state: AgentState) -> ActionResult:
     """
     Generate the final response based on the plan and user message.
+    Params:
+        text?: sends the provided text to the user as final response
+        filepath?: fetches the contents from the filepath and sends it to the user as final response
     """
     if not state.thought or (state.thought and state.thought.action.name != "python_runner"):
         return ActionResult(thought="", action="None", result="")
 
-    final_response = state.thought.action.params.get("text", "")
-    await state.stream_queue.put(StreamChunk(content="Finalizing Response...", step=StreamStep.DRAFT, step_title=""))
-    await state.stream_queue.put(StreamChunk(content=final_response, step=StreamStep.FINAL_RESPONSE, step_title="Finalizing Response"))
-
-    return ActionResult(thought=state.thought.thought, action="final_response", result=final_response)
+    final_response = state.thought.action.params.get("text")
+    if final_response:
+        await state.stream_queue.put(StreamChunk(content="Finalizing Response...", step=StreamStep.DRAFT, step_title=""))
+        await state.stream_queue.put(StreamChunk(content=final_response, step=StreamStep.FINAL_RESPONSE, step_title="Finalizing Response"))
+        return ActionResult(thought=state.thought.thought, action="final_response", result=final_response)
+    else:
+        file_path = state.thought.action.params.get("filepath")
+        if not file_path:
+            return ActionResult(thought=state.thought.thought, action="final_response", result="Need atleast one param (text | filepath)")
+        with open(file_path, "r") as f:
+            for line in f:
+                await state.stream_queue.put(StreamChunk(content=line, step=StreamStep.FINAL_RESPONSE, step_title="Sending response..."))
+                await asyncio.sleep(0.1)
+            return ActionResult(thought=state.thought.thought, action="final_response", result=f.read())
 
 
 async def intermediate_response_tool(state: AgentState) -> ActionResult:
@@ -223,5 +356,6 @@ tools = {
     "intermediate_response": intermediate_response_tool,
     "final_response": final_response_tool,
     "fetch_webpage": download_webpage_by_url,
-    "write_data_to_temp_memory": write_data_to_temp_memory,
+    "write_data_to_disk_memory": write_data_to_disk_memory,
+    "read_data_from_disk_memory": read_data_from_disk_memory,
 }
